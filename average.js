a895528d6c6674692e4e1ea31327450018094409
unknown <Amanpreetsinghbedi_19012722.cse@geu.ac.in>
2021-12-19 17:18:52 +0530

    Error handled

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 
-function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+function average(numbers) { 
+
+  let new_numbers = numbers.filter(element=>![NaN].includes(element));
+  return new_numbers.reduce((p, c)=> p + c, 0) / new_numbers.length;
 }
 
 module.exports = {average};
